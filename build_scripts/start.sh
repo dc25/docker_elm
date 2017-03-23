fafbdd36a7b10cb8b199190660b2bda193148c6b
Dave Compton <davecompton7@gmail.com>
2017-03-23 12:47:45 -0700

    restoing sudo enable for all users; removing /workarea from script invocation.

--- build_scripts/start.sh
+++ build_scripts/start.sh
@@ -9,12 +9,15 @@ if [[ "$user_exists" == 0 ]]; then
     echo "sshd started"
     sudo /usr/bin/svscan /services/ 
 else
-    echo "configuring daemontools for sshd"
-    /workarea/setup_sshd
+    echo "CONFIG: enabling sudo without password for all users"
+    echo '%sudo ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+    echo "CONFIG: configuring daemontools for sshd"
+    cd /workarea
+    ./setup_sshd
     echo "configuring user: $USER_NAME ..."
     adduser --disabled-password --gecos '' --home /workarea --uid $USER_ID $USER_NAME > /dev/null 2>&1 
     adduser $USER_NAME sudo > /dev/null 2>&1 
     chown $USER_NAME /workarea
     chmod go-w /workarea
-    su $USER_NAME -c "/workarea/user_configuration.sh \"$USER_KEY\""
+    su $USER_NAME -c "./user_configuration.sh \"$USER_KEY\""
 fi
